05faad07f0a6b195d7712cf3355ab42f8b988507
Alexey Makarov <alexeynkrv@gmail.com>
2021-08-08 23:32:58 +0700

    Initial Commit

---
+++ Homework_iOS_11_Skillbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_iOS_11_Skillbox
+//
+//  Created by Alexey Makarov on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
